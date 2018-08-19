f50a91968d262d0d2a566812e4b7bec7b5edc713
Peyton <41251008+pxgraham@users.noreply.github.com>
2018-08-18 20:56:15 -0700

    added phrase support

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,6 @@
 var startTemp = 0;
-var words = ['direct', 'pollution', 'Koran', 'enjoy', 'fat', 'entertainment', 'oppose', 'hard', 'justice', 'format', 'oil', 'launch', 'spite', 'imagine', 'urge', 'suffering', 'material', 'faint', 'dribble', 'sofa', 'harm', 'gloom', 'conference', 'warning', 'chief', 'discriminate', 'identification', 'bottom', 'pool', 'weak', 'valid', 'insight', 'deprivation', 'frighten', 'offense', 'flatware', 'lawyer', 'rear', 'imperial', 'cabin', 'curve', 'ankle', 'outfit', 'voice', 'beard', 'panel', 'joy', 'apathy', 'interest', 'appetite', 'road', 'socialist', 'man', 'welfare', 'star', 'panic', 'kinship', 'track', 'comfort', 'nun', 'way', 'economic', 'argument', 'unfortunate', 'monster', 'forest', 'conscience', 'fabricate', 'negotiation', 'fade', 'missile', 'crutch', 'method', 'bridge', 'resident', 'metal', 'authorise', 'bring', 'courage', 'development', 'broken', 'fitness', 'reflection', 'wreck', 'constant', 'penny', 'inquiry', 'firefighter', 'obligation', 'pioneer', 'solve', 'prisoner', 'humanity', 'mile', 'formulate', 'encourage', 'pawn', 'save', 'outlook', 'charter', 'document', 'element', 'javascript', 'hello', 'world', 'coding', 'cascade', 'develop', 'meowmix', 'eleven', 'power', 'knowledge', 'experience', 'experiment', 'control', 'africa', 'lemons', 'pomegranite', 'fusion', 'cold', 'elephant'];
+/* var words = ['direct', 'pollution', 'Koran', 'enjoy', 'fat', 'oil', 'launch', 'spite', 'imagine', 'urge', 'suffering', 'material', 'faint', 'dribble', 'sofa', 'harm', 'gloom', 'conference', 'warning', 'chief', 'discriminate', 'identification', 'bottom', 'pool', 'weak', 'valid', 'insight', 'deprivation', 'frighten', 'offense', 'flatware', 'lawyer', 'rear', 'imperial', 'cabin', 'curve', 'ankle', 'outfit', 'voice', 'beard', 'panel', 'joy', 'apathy', 'interest', 'appetite', 'road', 'socialist', 'man', 'welfare', 'star', 'panic', 'kinship', 'track', 'comfort', 'nun', 'way', 'economic', 'argument', 'unfortunate', 'monster', 'forest', 'conscience', 'fabricate', 'negotiation', 'fade', 'missile', 'crutch', 'method', 'bridge', 'resident', 'metal', 'authorise', 'bring', 'courage', 'development', 'broken', 'fitness', 'reflection', 'wreck', 'constant', 'penny', 'inquiry', 'firefighter', 'obligation', 'pioneer', 'solve', 'prisoner', 'humanity', 'mile', 'formulate', 'encourage', 'pawn', 'save', 'outlook', 'charter', 'document', 'element', 'javascript', 'hello', 'world', 'coding', 'cascade', 'develop', 'meowmix', 'eleven', 'power', 'knowledge', 'experience', 'experiment', 'control', 'africa', 'lemons', 'pomegranite', 'fusion', 'cold', 'elephant']; */
+var words = ['the cow jumped over the moon', 'legend says i am a boss', 'entertainment', 'oppose', 'hard', 'justice', 'format', 'you win some you lose some', 'element', 'formal', 'coding boot camp', 'this is fun'];
 var rand = Math.floor(Math.random() * words.length);
 var word = words[rand];
 var main = document.getElementById('main');
@@ -14,15 +15,28 @@ var letters = /[\w]/img;
 var time = 3000;
 var restarting = false;
 var img = document.getElementById('hangImg');
-console.log(word);
-
+var checkSpaces = true;
+var spaces = 0;
+if(word.match(/[\s]/g) !== null) {
+    spaces = word.match(/[\s]/g).length;
+}
+/* 
 for (var i = 0; i < word.length; i++) {
     var x = document.createElement('span');
     x.id = "letter" + i;
     x.textContent = '_';
     main.append(x);
 }
-
+ */
+for (var i = 0; i < word.length; i++) {
+    var x = document.createElement('span');
+    x.id = "letter" + i;
+    x.textContent = '_';
+    if(word[i] === " ") {
+        x.textContent = " ";
+    } 
+    main.append(x);
+}
 img.src = 'assets/images/hang0.png';
 
 document.addEventListener('keyup', function() {
@@ -63,6 +77,7 @@ document.addEventListener('keydown', function (e) {
     if(startTemp === 0) {
         document.getElementById('game').style.display = 'block';
         document.getElementById('pregame').style.display = 'none';
+        tempScore += spaces;
     }
     startTemp += 1;
     if(startTemp > 1) {
@@ -141,14 +156,27 @@ function tempWrite() {
     img.src = 'assets/images/hang0.png';
     rand = Math.floor(Math.random() * words.length);
     word = words[rand];
+    if(word.match(/[\s]/g) !== null) {
+        spaces = word.match(/[\s]/g).length;
+        tempScore += spaces;
+    }
     restarting = false;
     lettersUsed = [];
     main.innerHTML = '';
     document.getElementById('dec').innerText = '';
-    for (var i = 0; i < word.length; i++) {
+/*     for (var i = 0; i < word.length; i++) {
         var x = document.createElement('span');
         x.id = "letter" + i;
         x.textContent = '_';               
+        main.append(x);
+    } */
+    for (var i = 0; i < word.length; i++) {
+        var x = document.createElement('span');
+        x.id = "letter" + i;
+        x.textContent = '_';
+        if(word[i] === " ") {
+            x.textContent = " ";
+        } 
         main.append(x);
     }
     console.log(word);
